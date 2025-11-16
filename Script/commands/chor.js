f61fa7afa7d3a4aeca07176f9925ac865b094683
mahimasx <mrasif404as@gmail.com>
2025-11-16 22:55:56 +0600

    Update chor.js

--- Script/commands/chor.js
+++ Script/commands/chor.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "chor",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Mr Mahim",
  description: "scooby doo template memes",
 	commandCategory: "Picture",
 	usages: "...",
